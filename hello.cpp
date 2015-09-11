eb31b060abd733139e0ac9de96a0d2c9238d5128
RJS183 <suttonr@ELABSB138-10.easternct.edu>
2015-09-11 15:18:53 -0400

    Submission for CSC 450
    Edited the main program to prompt the user to enter his/her name (stored
    in a string object).

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-  dfdfd
-  cout << "Hello Class!\n\n";
+int main() {
 
-  return 0;
+	//cout << "Hello Class!\n\n";
+	string name;
+	cout << "What is your name? ";
+	cin >> name;
+	cout << "\n\nHello, " << name << ", my name is Alice.\n";
+	cout << "Wake up, Alice, this isn't Wonderland.  Damnit!";
+	return 0;
 }
 
